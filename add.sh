ac4070f2b2139d41eb9d25b4d38d0d63d24295d3
Lungu Stefan-Gabriel <hi@lungustefan.ro>
2025-04-13 21:35:42 +0300

    Revert "Update add.sh"
    This reverts commit 9fe103774b2778cfe2f4b5779e581257bfe5fa6d.

--- add.sh
+++ add.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-# Define certificate URLs and target filenames
-declare -A certs=(
-    ["LunguStefanRootCa"]="https://raw.githubusercontent.com/lungustefan/My-CA/refs/heads/main/LunguStefanRootCa.pem"
-    ["LunguStefan-DV-CA"]="https://raw.githubusercontent.com/lungustefan/My-CA/refs/heads/main/LunguStefan-DV-CA.pem"
-)
-
-# Download and rename PEMs to CRTs
-for name in "${!certs[@]}"; do
-    url="${certs[$name]}"
-    dest="/usr/local/share/ca-certificates/$name.crt"
-    curl -s -o "$dest" "$url"
-    echo "$name.pem downloaded to $dest"
-done
-
-# Update CA certificates
+curl -o /usr/local/share/ca-certificates/LunguStefanRootCa.crt https://raw.githubusercontent.com/lungustefan/My-CA/refs/heads/main/LunguStefanRootCa.pem
 update-ca-certificates
-
-echo "All certificates have been added to the trusted certificate store."
+echo "LunguStefanRootCa.pem has been added to the trusted certificate store."
